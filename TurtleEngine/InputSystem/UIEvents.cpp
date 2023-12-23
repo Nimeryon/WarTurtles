ad2a32233ffbcf6832574d8b4ef87e03fa1fe608
Romain Salha <salha.romain64@gmail.com>
2023-12-23 13:47:09 +0100

    Add primitive CmakeLists
    cmake ../ in build folder to launch hello world script

---
+++ TurtleEngine/InputSystem/UIEvents.cpp
@@ -0,0 +1,15 @@
+#include "UIEvents.h"
+
+
+void UIEventPublisher::SubscribeToEvent(const UIEventID& _ID, IUIEventSubscriber* pSubscriber)
+{
+	// TODO
+}
+void UIEventPublisher::UnSubscribeToEvent(const UIEventID& _ID, IUIEventSubscriber* pSubscriber)
+{
+	// TODO
+}
+void UIEventPublisher::TriggerEvent(const UIEventID& _ID)
+{
+	// TODO
+}
