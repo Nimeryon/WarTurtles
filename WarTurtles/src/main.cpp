05ace32c7b12bd2c82741675d334f6cc6f78ef03
Romain Salha <salha.romain64@gmail.com>
2023-12-25 16:41:18 +0100

    add backgound music to test mix+ loop

--- WarTurtles/src/main.cpp
+++ WarTurtles/src/main.cpp
@@ -13,6 +13,10 @@ class DemoScene : public Turtle::Scene
 	void OnCreate() override
 	{
 		Turtle::AudioManager::Instance().LoadSound("creeper",Turtle::SoundType::Sound,"../../Ressources/Audio/creeper.mp3");
+		Turtle::AudioManager::Instance().LoadSound("music",Turtle::SoundType::Music,"../../Ressources/Audio/music.mp3");
+		Turtle::AudioManager::Instance().SetGlobalVolume(Turtle::SoundType::Sound,100.f);
+		Turtle::AudioManager::Instance().SetGlobalVolume(Turtle::SoundType::Music,2.f);
+		Turtle::AudioManager::Instance().PlaySound("music",true);
 		Turtle::AudioManager::Instance().PlaySound("creeper");
 	}
 	void Gui(const Turtle::Time& deltaTime) override
