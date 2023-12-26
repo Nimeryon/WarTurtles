53b8b3b8bea9eea08b2c92bd8958c3acbaa403ad
Thomas <t.auriol46@gmail.com>
2023-12-26 02:41:13 +0100

    Audio manager syntax

--- TurtleEngine/include/AudioManager.h
+++ TurtleEngine/include/AudioManager.h
@@ -1,53 +1,54 @@
-﻿#pragma once
+﻿// AudioManager.h
+#ifndef AUDIO_MANAGER_H // include guard
+#define AUDIO_MANAGER_H
 #include <memory>
 #include <string>
 #include <unordered_map>
-
 #include <SFML/Audio.hpp>
-
-#include "Utils/Singleton.h"
-
 
 namespace Turtle
 {
-    using SoundBufferPtr  = std::unique_ptr<sf::SoundBuffer>;
-    using SoundPtr = std::unique_ptr<sf::Sound>;
-    using SoundEffect = std::string;
+using SoundBufferPtr  = std::unique_ptr<sf::SoundBuffer>;
+using SoundPtr = std::unique_ptr<sf::Sound>;
+using SoundEffect = std::string;
 
-    enum class SoundType{ Music,Sound};
-    
+enum class SoundType
+{
+	MUSIC,
+	SOUND
+};
 
-    
-    struct TurtleAudioInfo
-    {
-        SoundBufferPtr buffer;
-        SoundPtr sound;
-        SoundType soundType = SoundType::Sound;
-        float volume = 100.f;
-        float pitch = 1.f;
-    };
+struct TurtleAudioInfo
+{
+    SoundBufferPtr buffer;
+    SoundPtr sound;
+    SoundType soundType = SoundType::SOUND;
+    float volume = 100.f;
+    float pitch = 1.f;
+};
 
+class AudioManager final
+{
+public:
+    AudioManager();
+    AudioManager(AudioManager&) = delete;
+    ~AudioManager() = default;
     
-    class AudioManager final
-    {
-    public:
-        AudioManager() = default;
-        AudioManager(AudioManager&) = delete;
-        ~AudioManager() = default;
-        
-        void LoadSound(const SoundEffect& soundEffetName,const SoundType& type,const std::string& path);
-        void UnloadSound(const SoundEffect& soundEffetName);
-        void PlaySound(const SoundEffect& sound,bool loop = false);
-        void PauseSound(const SoundEffect& sound);
-        void StopSound(const SoundEffect& sound);
-        void SetPitch(const SoundEffect& sound,float pitch);
-        void SetRandomPitch(const SoundEffect& sound,float pitch);
-        void SetVolume(const SoundEffect& sound,float volume);
-        void SetGlobalVolume(const SoundType& soundType, float volume);
-    private:
-        std::unordered_map<SoundEffect,TurtleAudioInfo> _sounds;
-        float _musicVolume = 100.f;
-        float _soundVolume = 100.f;
-    };
+    void LoadSound(const SoundEffect& soundEffectName,const SoundType& type,const std::string& path);
+    void UnloadSound(const SoundEffect& soundEffectName);
+    void PlaySound(const SoundEffect& sound,bool loop = false);
+    void PauseSound(const SoundEffect& sound);
+    void StopSound(const SoundEffect& sound);
+    void SetPitch(const SoundEffect& sound,float pitch);
+    void SetRandomPitch(const SoundEffect& sound,float pitch);
+    void SetVolume(const SoundEffect& sound,float volume);
+    void SetGlobalVolume(const SoundType& soundType, float volume);
+
+private:
+    std::unordered_map<SoundEffect,TurtleAudioInfo> m_sounds;
+    float m_musicVolume;
+    float m_soundVolume;
+};
 }
 
+#endif /* AUDIO_MANAGER_H */
--- TurtleEngine/src/AudioManager.cpp
+++ TurtleEngine/src/AudioManager.cpp
@@ -1,36 +1,43 @@
 ﻿#include "AudioManager.h"
 
-void Turtle::AudioManager::LoadSound(const SoundEffect& soundEffetName,const SoundType& type,const std::string& path)
+Turtle::AudioManager::AudioManager() :
+	m_musicVolume(100.f),
+	m_soundVolume(100.f)
+{}
+
+void Turtle::AudioManager::LoadSound(const SoundEffect& soundEffectName,const SoundType& type,const std::string& path)
 {
     SoundBufferPtr buffer = std::make_unique<sf::SoundBuffer>();
     
-    if (buffer->loadFromFile(path)) {
+    if (buffer->loadFromFile(path))
+    {
         SoundPtr sound = std::make_unique<sf::Sound>();
         sound->setBuffer(*buffer);
         TurtleAudioInfo infos;
         infos.buffer = std::move(buffer);
         infos.sound = std::move(sound);
         infos.soundType = type;
-        _sounds[soundEffetName] = std::move(infos);
+        m_sounds[soundEffectName] = std::move(infos);
     }
 }
 
-void Turtle::AudioManager::UnloadSound(const SoundEffect& soundEffetName)
+void Turtle::AudioManager::UnloadSound(const SoundEffect& soundEffectName)
 {
-    auto it = _sounds.find(soundEffetName);
-    if (it != _sounds.end())
+    auto it = m_sounds.find(soundEffectName);
+    if (it != m_sounds.end())
     {
-        _sounds.erase(soundEffetName);
+        m_sounds.erase(soundEffectName);
     }
 }
 
 void Turtle::AudioManager::PlaySound(const SoundEffect& sound, bool loop)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         const TurtleAudioInfo& infos = it->second;
-        float gloablVolume = infos.soundType == SoundType::Music?_musicVolume:_soundVolume;
-        infos.sound->setVolume(infos.volume*gloablVolume/100.f);
+        const float globalVolume = infos.soundType == SoundType::MUSIC ? m_musicVolume : m_soundVolume;
+        infos.sound->setVolume(infos.volume * globalVolume / 100.f);
         infos.sound->setLoop(loop);
         infos.sound->setPitch(infos.pitch);
         infos.sound->play();
@@ -39,40 +46,45 @@ void Turtle::AudioManager::PlaySound(const SoundEffect& sound, bool loop)
 
 void Turtle::AudioManager::PauseSound(const SoundEffect& sound)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         it->second.sound->pause();
     }
 }
 
 void Turtle::AudioManager::StopSound(const SoundEffect& sound)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         it->second.sound->stop();
     }
 }
 
 void Turtle::AudioManager::SetPitch(const SoundEffect& sound, float pitch)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         it->second.pitch = pitch;
     }
 }
 
 void Turtle::AudioManager::SetRandomPitch(const SoundEffect& sound, float pitch)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         it->second.pitch = pitch;
     }
 }
 
 void Turtle::AudioManager::SetVolume(const SoundEffect& sound, float volume)
 {
-    auto it = _sounds.find(sound);
-    if (it != _sounds.end()) {
+    auto it = m_sounds.find(sound);
+    if (it != m_sounds.end())
+    {
         it->second.volume = volume;
     }
 }
@@ -80,12 +92,12 @@ void Turtle::AudioManager::SetVolume(const SoundEffect& sound, float volume)
 void Turtle::AudioManager::SetGlobalVolume(const SoundType& soundType, float volume)
 {
     switch(soundType){
-        case SoundType::Music:
-            _musicVolume = volume;
-            break;
-        case SoundType::Sound:
-            _soundVolume = volume;
-            break;
-        default:;
+    case SoundType::MUSIC:
+        m_musicVolume = volume;
+        break;
+    case SoundType::SOUND:
+        m_soundVolume = volume;
+        break;
+    default: break;
     }
 }
