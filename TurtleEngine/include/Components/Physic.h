6e4e8d9d05c70025307c532210af5895ec00adb0
theofor1 <theo.fs2000@gmail.com>
2024-02-23 15:55:35 +0100

    Begining of Physics

---
+++ TurtleEngine/include/Components/Physic.h
@@ -0,0 +1,33 @@
+// Physic.h
+#ifndef PHYSIC_H
+#define PHYSIC_H
+#include "Components/Component.h"
+#include "Types/Vector2.h"
+
+namespace Turtle
+{
+	class Physic : public Component
+	{
+	public:
+		Physic() = delete;
+		Physic(GameObject* parent, const std::string& name,Vector2f initialVelocity, Vector2f initialAcceleration, float mass = 1, float friction = 0.2, float bounciness = 0.1);
+
+		void FixedUpdate(const Time& fixedTime) override;
+		void OnActivate() override;
+		void OnDeactivate() override;
+		void OnEnabled() override;
+		void OnDisabled() override;
+
+		Vector2f GetLocalGravity();
+		Vector2f GetAllForces();
+
+		Vector2f m_velocity;
+		Vector2f m_acceleration;
+		float m_mass;
+		float m_friction; // 0 to 1 : 0 = no friction ( like in space ), 1 = will not be able to move
+		float m_bounciness; // 0 to 1 : 0 = no bounce ( will stop if encounter a wall ), 1 = will bounce at same acceleration
+		Vector2f m_localGravity; // local gravity, i.e magnet...
+	};
+}
+#endif // !PHYSIC_H
+
---
+++ TurtleEngine/include/Managers/PhysicManager.h
@@ -0,0 +1,26 @@
+// Physic.h
+#ifndef PHYSICMANAGER_H
+#define PHYSICMANAGER_H
+#include "Types/Vector2.h"
+#include "Utils/Time.h"
+#include "GameObjects/GameObject.h"
+#include "Components/Physic.h"
+#include <Components/Transform.h>
+
+namespace Turtle
+{
+	class PhysicManager final
+	{
+	public:
+		explicit PhysicManager(Vector2f globalGravity);
+
+		void ComputeNewPositionFor(Physic& ObjectPhysicComponent, Transform& ObjectTransformComponent, const Time& fixedTime);
+		Vector2f GetGlobalGravity() const;
+
+		void SetGlobalGravity(Vector2f newGravity);
+		
+	private:
+		Vector2f m_globalGravity;
+	};
+}
+#endif // !PHYSICMANAGER_H
