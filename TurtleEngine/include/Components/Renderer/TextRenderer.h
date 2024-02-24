f1393c0f3daf8e0838543098aa406e4e55f6c341
Romain Salha <salha.romain64@gmail.com>
2024-02-24 19:34:23 +0100

    Add Text Renderer V1
    (+ remove unused IDrawable)

--- TurtleEngine/include/Components/Renderer/TextRenderer.h
+++ TurtleEngine/include/Components/Renderer/TextRenderer.h
@@ -1,14 +1,26 @@
-﻿#ifndef TEXTRENDERER_H // include guard
-#define TEXTRENDERER_H
+﻿#ifndef TEXT_RENDERER_H
+#define TEXT_RENDERER_H
+
+#include <SFML/Graphics.hpp>
 #include "Components/Component.h"
-#include "Interfaces/IDrawable.h"
 
 namespace Turtle
 {
-    class TextRenderer final : public Component, public IDrawable
+    class TextRenderer final : public Component
     {
+    public:
+        explicit TextRenderer(GameObject* parent, const std::string& name = "TextRenderer");
+
+        void SetFont(const sf::Font* font);
+        void SetText(const std::string& text);
+        void SetCharacterSize(unsigned int size);
+        void SetColor(const sf::Color& color);
+        
+        void Draw(sf::RenderWindow& window) override;
+
+    private:
+        sf::Text m_text;
     };
-}
+} // namespace Turtle
 
-#endif
-
+#endif /* TEXT_RENDERER_H */
--- TurtleEngine/src/Components/Renderer/TextRenderer.cpp
+++ TurtleEngine/src/Components/Renderer/TextRenderer.cpp
@@ -1,1 +1,34 @@
 ﻿#include "Components/Renderer/TextRenderer.h"
+#include "Components/Transform.h"
+#include "GameObjects/GameObject.h"
+
+namespace Turtle
+{
+    TextRenderer::TextRenderer(GameObject* parent, const std::string& name) : Component(parent, name) {}
+
+    void TextRenderer::SetFont(const sf::Font* font)
+    {
+        m_text.setFont(*font);
+    }
+
+    void TextRenderer::SetText(const std::string& text)
+    {
+        m_text.setString(text);
+    }
+
+    void TextRenderer::SetCharacterSize(unsigned int size)
+    {
+        m_text.setCharacterSize(size);
+    }
+
+    void TextRenderer::SetColor(const sf::Color& color)
+    {
+        m_text.setFillColor(color);
+    }
+
+    void TextRenderer::Draw(sf::RenderWindow& window)
+    {
+        const Transform* transform = m_parent->GetTransform();
+        window.draw(m_text, transform->GetTransformMatrix());
+    }
+}
