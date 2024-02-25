11afab1f0382237fc035dd4d1753bca8bc7d5bd4
Romain Salha <salha.romain64@gmail.com>
2024-02-25 15:27:42 +0100

    Add Shape Renderer V1

---
+++ TurtleEngine/include/Components/Renderer/ShapeRenderer.h
@@ -0,0 +1,27 @@
+﻿// ShapeRenderer.h
+#ifndef SHAPE_RENDERER_H
+#define SHAPE_RENDERER_H
+
+#include <SFML/Graphics.hpp>
+#include "Components/Component.h"
+
+namespace Turtle
+{
+    template <typename ShapeType>
+    class ShapeRenderer final : public Component
+    {
+    public:
+        explicit ShapeRenderer(GameObject* parent, const std::string& name = "ShapeRenderer");
+
+        void SetShape(const ShapeType& shape);
+        void SetColor(const sf::Color& color);
+        void Draw(sf::RenderWindow& window) override;
+
+    private:
+        ShapeType m_shape;
+    };
+} // namespace Turtle
+
+#include "ShapeRenderer.hxx" // Include the implementation file
+
+#endif /* SHAPE_RENDERER_H */
---
+++ TurtleEngine/include/Components/Renderer/ShapeRenderer.hxx
@@ -0,0 +1,33 @@
+﻿// ShapeRenderer.hxx
+#ifndef SHAPE_RENDERER_HXX
+#define SHAPE_RENDERER_HXX
+#include "ShapeRenderer.h"
+
+namespace Turtle
+{
+    class Transform;
+}
+
+template <typename ShapeType>
+Turtle::ShapeRenderer<ShapeType>::ShapeRenderer(GameObject* parent, const std::string& name) : Component(parent, name) {}
+
+template <typename ShapeType>
+void Turtle::ShapeRenderer<ShapeType>::SetShape(const ShapeType& shape)
+{
+    m_shape = shape;
+}
+
+template <typename ShapeType>
+void Turtle::ShapeRenderer<ShapeType>::SetColor(const sf::Color& color)
+{
+    // Assuming that the shape type has a setFillColor method
+    m_shape.setFillColor(color);
+}
+
+template <typename ShapeType>
+void Turtle::ShapeRenderer<ShapeType>::Draw(sf::RenderWindow& window)
+{
+    const Transform* transform = this->m_parent->GetTransform();
+    window.draw(m_shape, transform->GetTransformMatrix());
+}
+#endif
